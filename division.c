9931e4e24d55af6c43cbce142a48e1f294f7ac97
Shagun-kumar01 <lakhan4773@gmail.com>
2026-06-16 19:06:44 +0530

    Add division operator demo

---
+++ division.c
@@ -0,0 +1,18 @@
+// Program to demonstrate Division Operator 
+#include <stdio.h>
+
+int main () {
+  // Declared total pieces of cake
+  int Total_cake_pieces = 20;
+
+  // total friends
+  int No_of_friends = 10;
+  
+  // Distribute_cake_between_friends
+  int Per_person_pieces_of_cake = Total_cake_pieces / No_of_friends;
+
+printf("Every Friend received pieces of cake: %d\n", Per_person_pieces_of_cake);
+
+return 0;
+
+}
